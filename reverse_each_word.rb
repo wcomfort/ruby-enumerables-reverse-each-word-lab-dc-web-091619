12d4fd1f8e6401117d4059a78f348dfe12d7cd1c
Will Comfort <comfortwb21@gmail.com>
2019-09-05 19:55:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(string)
 return reversed.join("")
 end
 
+def reverse_each_word(string)
+  string = string.split("")
+  reversed = []
+  string.collect do |i|
+    reversed.push(i)
+  end
+  return reversed.join("")
+end 
