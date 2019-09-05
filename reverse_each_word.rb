7b7cbcb24c9247abc9437f6565355daa379737e0
Will Comfort <comfortwb21@gmail.com>
2019-09-05 19:58:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,16 +2,16 @@
  # string = string.split("")
   #reversed = []
   #string.each do |i|
-    reversed.push(i) 
-  end 
-return reversed.join("")
-end
+   # reversed.push(i) 
+  #end 
+#return reversed.join("")
+#end
 
 def reverse_each_word(string)
-  string = string.split("")
-  reversed = []
+  words = string.split(" ")
+ # reversed = []
   string.collect do |i|
     reversed.push(i)
   end
-  return reversed.join("")
+  #return reversed.join("")
 end 
